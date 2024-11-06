645eb64c98da538352f79e1d32c6ca69f8d4a410
nguyenhung1402 <21522118@ms.uit.edu.vn>
2024-11-06 22:50:03 +0700

    add jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -36,13 +36,7 @@ pipeline {
       }
     }
 
-    stage('Deploying App to Kubernetes') {
-      steps {
-        script {
-          kubernetesDeploy(configs: "deploymentservice.yml", kubeconfigId: "kubernetes")
-        }
-      }
-    }
+    
 
   }
 
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 8800;
 
 const server = app.listen(PORT, () => {
   console.log(`Test`);
-  console.log(`Dev Server test running on port: ${PORT}`);
+  console.log(`Dev Server running on port: ${PORT}`);
+  console.log(`New image_2`);
 });
 
 export default server;
